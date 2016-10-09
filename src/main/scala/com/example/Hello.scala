bec3aee4a320c4e130eedfad693c8e106c2b85f4
shigemk2 <i.am.shige@gmail.com>
2016-10-09 17:40:51 +0900

    Add

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,21 @@
 package com.example
 
+import akka.actor.ActorSystem
+import com.rabbitmq.client.ConnectionFactory
+import com.github.sstone.amqp.{ChannelOwner, ConnectionOwner, Amqp}
+import com.rabbitmq.client.AMQP.Queue
+import com.github.sstone.amqp.Amqp._
+
+import scala.concurrent.duration._
+
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val system = ActorSystem("mySystem")
+    val connFactory = new ConnectionFactory()
+    connFactory.setUri("amqp://guest:guest@localhost/%2F")
+    val conn = system.actorOf(ConnectionOwner.props(connFactory, 1 second))
+    val channel = ConnectionOwner.createChildActor(conn, ChannelOwner.props())
+
+    channel ! DeclareQueue(QueueParameters("my_queue", passive = false, durable = false, exclusive = false, autodelete = true))
   }
 }
